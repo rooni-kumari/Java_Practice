42c698cec7ef5d9a9944299af51467bb0d4d6fee
rooni-kumari <kumarirooni44@gmail.com>
2026-02-09 11:20:56 +0530

    practice

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo{
+    public static void main(String[] args)
+    {
+        byte a=10;
+        short b=20;
+        int c=30;
+        System.out.println("byte "+a+" short "+b);
+        System.out.println("this is demo");
+    }
+}
