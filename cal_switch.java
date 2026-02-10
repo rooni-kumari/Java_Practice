6dae27e3725141ea8bc7fe7e6f9b44d13f74bcd4
rooni-kumari <kumarirooni44@gmail.com>
2026-02-10 23:29:36 +0530

    cal_with switch

---
+++ cal_switch.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+class Calculator{
+    public static void main(String arg[])
+    {
+        int a=15;
+        int b=10;
+        
+        Scanner sc= new Scanner(System.in);
+        
+        int c=0;
+        System.out.println("choice operators 1.+ ,2. - ,3. * ,4. / ,5. % ");
+        int choice= sc.nextInt();
+
+        switch(choice)
+        {
+            case 1:
+                c=a+b;
+                break;
+            case 2:
+                c=a-b;
+                break;
+            case 3:
+                c=a*b;
+                break;
+            case 4:
+                c=a/b;
+                break;
+            case 5:
+                c=a%b;
+                break;
+            default:
+                System.out.println("enter input is not correct");
+        }
+        System.out.println("output is :"+c);
+    }
+}
