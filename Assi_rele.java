03a9fa59752baafc4a901bb2f37116bce295984e
rooni-kumari <kumarirooni44@gmail.com>
2026-02-16 12:04:00 +0530

    assignment and relational

---
+++ Assi_rele.java
@@ -0,0 +1,15 @@
+class Assi_rele{
+    public static void main(String arg[])
+    {
+        int a=4;
+        int b=5;
+        if(a>b)
+        {
+            System.out.println("A is already greater ");
+        }
+        else{
+            a=b;
+            System.out.println("B is greater now A is greater :"+a);
+        }
+    }
+}
