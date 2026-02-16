9d40821adcfd862ddf753869e72741503e1b6875
rooni-kumari <kumarirooni44@gmail.com>
2026-02-16 12:07:31 +0530

    unary operator

---
+++ Unary.java
@@ -0,0 +1,9 @@
+class Unary{
+    public static void main(String arg[])
+    {
+        int a=2;
+        System.out.println("A is before unary operator "+a);
+        ++a;
+        System.out.println("A is after operator "+a);
+    }
+}
