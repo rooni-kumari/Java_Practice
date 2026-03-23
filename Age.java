2a2af551ead5ad70827332483f3106ad61945e40
rooni-kumari <kumarirooni44@gmail.com>
2026-03-23 10:40:39 +0530

    throw

---
+++ Age.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Age{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter age : ");
+        int age = sc.nextInt();
+        if(age<0) throw new ArithmeticException();
+        
+        
+        System.out.println("after exception");
+    }
+}
